29509d0be4ca9d1af3f365470582803e8534aac7
arthur.sequeira <arthurbowens18@gmail.com>
2024-11-06 11:18:18 -0300

    update

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,8 @@
+import { Router } from '@angular/router';
+import { LoginService } from '../shared/service/login.service';
 import { UsuarioDTO } from './../shared/model/dto/UsuarioDTO';
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
 //import { UsuarioDTO } from '../shared/model/dto/UsuarioDTO';
 
 @Component({
@@ -7,14 +10,34 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
 
-constructor(usuarioDTO: UsuarioDTO){}
+  public usuarioDTO = new UsuarioDTO();
 
-//function realizarLogin(UsuarioDTO: UsuarioDTO) {
+  constructor(
+    private loginService: LoginService,
+    private router: Router
+  ) {}
 
-//}
+  public realizarLogin() {
+    this.loginService.autenticar(this.usuarioDTO).subscribe(
+      (token: any) => {
+        Swal.fire('Sucesso', 'Usuário autenticado com sucesso', 'success');
+        this.router.navigate(['/home']);
+      },
+      (erro) => {
+        var mensagem: string;
+        if (erro.status == 401) {
+          mensagem = 'Usuário ou senha inválidos, tente novamente';
+        } else {
+          mensagem = erro.error.mensagem;
+        }
 
+        Swal.fire('Erro', mensagem, 'error');
+        // Swal.fire('Erro', erro.error.mensagem, 'error');
+      }
+    );
+  }
 }
